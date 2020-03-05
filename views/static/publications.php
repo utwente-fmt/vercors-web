4f99d22038f87d89fdabee27ca32d2c715131492
Pieter Bos <info@pieterbos.me>
2020-03-05 17:07:05 +0100

    fix publications

--- views/static/publications.php
+++ views/static/publications.php
@@ -11,7 +11,7 @@
 
             <!-- <script src="https://bibbase.org/show?bib=http://utwente-fmt.github.io/vercors/references.bib&jsonp=1"></script> -->
             <!-- NO CACHE SCRIPT FOR UPDATING WEB PAGE: -->
-            <script src="https://bibbase.org/show?bib=http://utwente-fmt.github.io/vercors/references.bib&jsonp=1&nocache=1"></script>
+            <script src="https://bibbase.org/show?bib=https://vercors.ewi.utwente.nl/references.bib&jsonp=1&nocache=1"></script>
         </section>
     </div>
 </div>
