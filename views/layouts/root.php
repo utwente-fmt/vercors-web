ff97970a810a1ee82f1bb435251341f850299bab
Pieter Bos <info@pieterbos.me>
2020-03-19 15:08:30 +0100

    rudimentary reponsive-design hamburger menu; drop skel

--- views/layouts/root.php
+++ views/layouts/root.php
@@ -20,6 +20,7 @@ AppAsset::register($this);
     <head>
         <?= Html::csrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?> · VerCors Tool FMT | UTwente </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
         <meta name="description" content=""/>
         <meta name="keywords" content=""/>
@@ -28,76 +29,68 @@ AppAsset::register($this);
     </head>
     <body class="no-sidebar">
     <?php $this->beginBody() ?>
-    <div id="skel-layers-wrapper">
-        <div id="header">
-            <div class="container">
-
-                <!-- Logo -->
-                <h1><?= Html::a('The VerCors Verifier', ['static/index'], ['id' => 'logo']) ?></h1>
-
-                <!-- Nav -->
-                <nav id="nav">
+    <div id="header">
+        <h1 id="logo" style="position: relative">
+            <span id="hamburger"
+                  onclick="document.querySelector('#nav').classList.toggle('open')"
+                  style="position: absolute; padding: 0 0.75em; left: 0; top: 0; line-height: 2.75em; cursor: pointer;"
+                  class="fa fa-bars"></span>
+            <?= Html::a('The VerCors Verifier', ['static/index']) ?>
+        </h1>
+        <nav id="nav">
+            <ul>
+                <li>
+                    <a href="">Getting Started</a>
                     <ul>
+                        <li><?= Html::a('Installation Guide', ['static/installation']) ?></li>
+                        <li><?= Html::a('Tool Overview', ['static/architecture']) ?></li>
+                        <li><?= Html::a('Tutorials', ['static/tutorials']) ?></li>
                         <li>
-                            <a href="">Getting Started</a>
-                            <ul>
-                                <li><?= Html::a('Installation Guide', ['static/installation'], ['target' => '_blank']) ?></li>
-                                <li><?= Html::a('Tool Overview', ['static/architecture'], ['target' => '_self']) ?></li>
-                                <li><?= Html::a('Tutorials', ['static/tutorials'], ['target' => '_blank']) ?></li>
-                                <!--
-                                <li><a href="https://github.com/utwente-fmt/vercors/tree/master/examples" target="_blank">Examples</a></li>
-                                -->
-                                <li>
-                                    <a href="https://github.com/utwente-fmt/vercors/wiki" target="_blank">VerCors with
-                                        IDEs</a>
-
-                                </li>
-                                <li><a href="https://github.com/utwente-fmt/vercors/issues" target="_blank">Issue
-                                        Tracker</a></li>
-                            </ul>
+                            <a href="https://github.com/utwente-fmt/vercors/wiki" target="_blank">
+                                VerCors with IDEs
+                                <span class="fa fa-external-link" style="font-size: 10pt"></span>
+                            </a>
                         </li>
-                        <li><?= Html::a('Showcases', ['site/examples'], ['target' => '_blank']) ?></li>
-                        <li><?= Html::a('News', ['news/index']) ?></li>
-                        <li><?= Html::a('Publications', ['static/publications']) ?></li>
-                        <li><?= Html::a('About', ['static/about']) ?>
-                            <ul>
-                                <li><?= Html::a('VerCors Team', ['static/about', '#' => 'Team']) ?></li>
-                                <li><?= Html::a('Contact', ['static/about', '#' => 'Contact']) ?></li>
-                                <li><?= Html::a('Credits', ['static/about', '#' => 'Credits']) ?></li>
-                                <li><?= Html::a('License', ['static/license']) ?></li>
-                            </ul>
+                        <li>
+                            <a href="https://github.com/utwente-fmt/vercors/issues" target="_blank">
+                                Issue Tracker
+                                <span class="fa fa-external-link" style="font-size: 10pt"></span>
+                            </a>
                         </li>
                     </ul>
-                </nav>
-
-                <!-- Banner -->
-                <div id="banner">
-                    <div class="container">
-                        <section>
-                            <?php if(isset($this->blocks['banner'])) { ?><?= $this->blocks['banner'] ?><?php } ?>
-                            <a href="https://github.com/utwente-fmt/vercors" class="button alt" target="_blank">View on
-                                Github</a>
-                            <a href="/try_online" class="button alt">Try VerCors Online</a>
-                        </section>
-                    </div>
+                </li>
+                <li><?= Html::a('Showcases', ['site/examples']) ?></li>
+                <li><?= Html::a('News', ['news/index']) ?></li>
+                <li><?= Html::a('Publications', ['static/publications']) ?></li>
+                <li>
+                    <?= Html::a('About', ['static/about']) ?>
+                    <ul>
+                        <li><?= Html::a('VerCors Team', ['static/about', '#' => 'Team']) ?></li>
+                        <li><?= Html::a('Contact', ['static/about', '#' => 'Contact']) ?></li>
+                        <li><?= Html::a('Credits', ['static/about', '#' => 'Credits']) ?></li>
+                        <li><?= Html::a('License', ['static/license']) ?></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <div class="container">
+            <!-- Banner -->
+            <div id="banner">
+                <div class="container">
+                    <section>
+                        <?php if(isset($this->blocks['banner'])) { ?><?= $this->blocks['banner'] ?><?php } ?>
+                        <a href="https://github.com/utwente-fmt/vercors" class="button alt" target="_blank">View on
+                            Github</a>
+                        <a href="/try_online" class="button alt">Try VerCors Online</a>
+                    </section>
                 </div>
             </div>
         </div>
     </div>
 
-    <!-- Fake Banner for small screens -->
-    <div id="fakeheader">
-        <section style="text-align: center; padding:5%; color:#eee; background-image: url(../../web/images/Vercors_Original.jpg); background-repeat: no-repeat; background-size: cover; box-shadow: inset 0 0 0 1000px rgba(0,0,0,.2);">
-            <?php if(isset($this->blocks['banner'])) { ?><?= $this->blocks['banner'] ?><?php } ?>
-            <a href="https://github.com/utwente-fmt/vercors" class="button alt" target="_blank">View on Github</a>
-            <a href="/try_online" class="button alt">Try VerCors Online</a>
-        </section>
-    </div>
-
     <?= $content ?>
 
     <div id="footer">
-
         <!-- Copyright -->
         <div class="row">
             <a href="https://fmt.ewi.utwente.nl" target="_blank"><img src="/images/FMT logo.png" alt=""></a>
